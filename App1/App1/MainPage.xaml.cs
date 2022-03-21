f10fd7a664db5c5a3d58b61cd2e7f9f3fbd15ca8
Marthin <Marthin Silalahi>
2022-03-21 08:41:34 +0700

    Setup project

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            //if (EntryUsername.Text == null || EntryUsername.Text == "")
+            //{
+            //    await DisplayAlert("Warning", "Username kosong", "OK");
+            //}
+
+            if (string.IsNullOrEmpty(EntryUsername.Text))
+            {
+                await DisplayAlert("Warning", "Username kosong", "OK");
+            }
+            else if (string.IsNullOrEmpty(EntryPassword.Text))
+            {
+                await DisplayAlert("Warning", "Password kosong", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Success", "User valid", "OK");
+            }
+        }
+    }
+}
